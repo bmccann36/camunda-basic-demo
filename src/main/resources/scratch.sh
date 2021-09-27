21c165d3cf2337cc02d68c8a0b9571ee3af2f83f
BRIAN MCCANN <extern.mccann@mbusa.com>
2021-09-27 08:24:36 -0400

    initial commit

---
+++ src/main/resources/scratch.sh
@@ -0,0 +1,4 @@
+
+
+mvn install -DskipTests=true && cp target/camunda-demo-0.1.0-SNAPSHOT.war \
+~/software-distros/camunda-bpm-tomcat-7.15.0/server/apache-tomcat-9.0.43/webapps/camunda-demo-0.1.0-SNAPSHOT.war
