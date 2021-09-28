59288ea9d5161353edef75e9504f81b505c6f481
mccann-b <brian@cedrus.digital>
2021-09-28 07:14:49 -0400

    delete some files to have a clean start point

--- src/main/java/com/cedrus/camundademo/ProcessRequestDelegate.java
+++ src/main/java/com/cedrus/camundademo/ProcessRequestDelegate.java
@@ -23,10 +23,7 @@ import java.util.logging.Logger;
 
 public class ProcessRequestDelegate implements JavaDelegate {
 
-  private final static Logger LOGGER = Logger.getLogger("LOAN-REQUESTS");
-
   public void execute(DelegateExecution execution) throws Exception {
-    LOGGER.info("Processing request by '"+execution.getVariable("customerId")+"'...");
   }
 
 }
